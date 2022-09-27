becebec7c7b5f0aaafd5a8739e89a64945fd6b3d
Maria Angelica <mariancademico@gmail.com>
2022-09-27 00:33:13 -0400

    verificando o email

--- liguagem-formais-automatos/maria_angelica_regex_ipv4_email.c
+++ liguagem-formais-automatos/maria_angelica_regex_ipv4_email.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+tam = 0;
+
 typedef struct no_lista{
     char letras;
     struct no_lista *prox;
@@ -8,17 +10,13 @@ typedef struct no_lista{
 }NOLISTA;
 
 typedef struct lista{
-    char vert;
-    char estudante;
-    char professor;
-    int tam;
+    int valor;
     NOLISTA *inicio;
     NOLISTA *fim;
-    NOLISTA *p_adj;
 }LISTA;
 
 NOLISTA *cria_noLista (char caracteres){
-    NOLISTA *no = (NOLISTA*)calloc(1, sizeof(NOLISTA));
+    NOLISTA *no = (NOLISTA*)malloc(sizeof(NOLISTA));
     
     no -> letras = caracteres;
     no -> prox = NULL;
@@ -26,10 +24,11 @@ NOLISTA *cria_noLista (char caracteres){
 }
 
 LISTA *cria_Lista(){
-    LISTA *list = (LISTA*)calloc(1, sizeof(LISTA));
+    LISTA *list = (LISTA*)malloc(sizeof(LISTA));
     
     list -> inicio = NULL;
     list -> fim = NULL;
+    list -> valor = 1;
 }
 
 void adiconar_dados(LISTA **l, char carcter, int indice){
@@ -43,9 +42,13 @@ void adiconar_dados(LISTA **l, char carcter, int indice){
         l[indice]->fim->prox = aux;
         l[indice]->fim = l[indice]->fim->prox;
     }
+    tam++;
 }
 
-void ler_arquivo(LISTA **l){
+void ler_arquivo_email(LISTA **l){
+    int cont = 0;
+    char var_email;
+
     FILE *arquivo = fopen("email.csv","r");
 
     if (arquivo == NULL){
@@ -55,15 +58,14 @@ void ler_arquivo(LISTA **l){
         printf("DEU BOM A LEITURA \n");
     }
 
-    l[0] = cria_Lista();
+    l[cont] = cria_Lista();
     char caracter = fgetc(arquivo);
-    int cont = 0;
-    char var_email;
 
     while (caracter != EOF){
         if (caracter == ','){
             cont ++;
             l[cont] = cria_Lista();
+            l[cont] -> valor = 1;
             caracter = fgetc(arquivo);
             
         }
@@ -74,18 +76,85 @@ void ler_arquivo(LISTA **l){
     fclose (arquivo);
 }
 
-void verifiacar_email(LISTA **l, char caracter){
+void verifiacar_email(LISTA **l){
     //NOLISTA *aux = cria_noLista(caracter);
 
-    char letra_email[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    int cont, i;
+    int cont_ponto, val, flag;
+    cont_ponto = 0;
+    val = 0;
+    flag = 0;
 
-    for(i = 0; i < 100; i++){
+    int cont, j, i, k;
+    cont = 0;
+    j = 0;
+    i = 0;
+
+    char estudante[23] = {"@estudante.ifms.edu.br"};
+    char professor[23] = {"@ifms.edu.br"};
+
+    for(i = 0; i < tam; i++){
         NOLISTA *aux = l[i] -> inicio;
 
+        while (aux){
+            char *email = (char*)calloc(80, sizeof(char));
+
+            while (aux -> letras != '@'){
+                email[j] = aux->letras;
+                j++;
+                val++;
+                aux = aux->prox;
+
+            }
+
+            for(k = 0; k < val; k++){
+                if(email[k] == '.'){
+                    cont_ponto++;
+                }
+            }
+
+            if(cont_ponto == 1){
+                for(k = 0; k < val -1; cont_ponto++){
+                    if(email[k] >= 'a' && email[k] <= 'z' || email[k] == '.'){
+                        exit;
+                    }
+                    else{
+                        flag++;
+                    }
+                }
+            }
+            free(email);
+
+            if(email != 0){
+                l[i] -> valor = 0;
+            }
+
+            char *emial_final = (char*)calloc(80, sizeof(char));
+
+            while (aux != NULL){
+                emial_final[cont] = aux->letras;
+                cont++;
+                aux = aux->prox;
+            }
+
+            if(strcmp(email, estudante) == 0){
+                if(l[i]->valor != 0){
+                    l[i]->valor = 2;
+                }
+            }
+            if(strcmp(email, professor) == 0){
+                if(l[i]->valor != 0){
+                    l[i]->valor = 3;
+                }
+            }
+            else{
+                l[i]->valor = 0;
+            }
+
+            break;
+            free(emial_final);            
+        }
+        printf(l, i);
     }
-
-
         
 }
 
@@ -108,6 +177,8 @@ void imprimir(LISTA **l){
 }
 
 int main (void){
+
+    printf("NO MOMENTO O CODIGO ESTA SOMENTE FAZENDO VALIDAÇÃO DE EMIAL");
 
     LISTA **v_email = (LISTA**)malloc(100* sizeof(LISTA*));
     ler_arquivo(v_email);
