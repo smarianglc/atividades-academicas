c4bbc438c7f24d7b5f70f3dd5de814d3b333d15b
Maria Angelica <mariancademico@gmail.com>
2022-09-27 22:40:23 -0400

    leitura de arquivos e validacao email

--- liguagem-formais-automatos/maria_angelica_regex_ipv4_email.c
+++ liguagem-formais-automatos/maria_angelica_regex_ipv4_email.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-tam = 0;
+#include<string.h>
 
 typedef struct no_lista{
     char letras;
@@ -30,8 +29,8 @@ LISTA *cria_Lista(){
     list -> fim = NULL;
     list -> valor = 1;
 }
-
-void adiconar_dados(LISTA **l, char carcter, int indice){
+int tam = 0;
+void adiconar_dados(char carcter, int indice, LISTA **l){
     NOLISTA *aux = cria_noLista(carcter);
 
     if(l[indice]->inicio == NULL){
@@ -46,10 +45,9 @@ void adiconar_dados(LISTA **l, char carcter, int indice){
 }
 
 void ler_arquivo_email(LISTA **l){
+    FILE *arquivo = fopen("email.csv","r");
     int cont = 0;
     char var_email;
-
-    FILE *arquivo = fopen("email.csv","r");
 
     if (arquivo == NULL){
         printf("ERRO AO LER O ARQUIVO \n");
@@ -59,21 +57,51 @@ void ler_arquivo_email(LISTA **l){
     }
 
     l[cont] = cria_Lista();
-    char caracter = fgetc(arquivo);
+    //char caracter = fgetc(arquivo);
 
-    while (caracter != EOF){
-        if (caracter == ','){
+    while (var_email != EOF){
+        if (var_email == ','){
             cont ++;
             l[cont] = cria_Lista();
-            l[cont] -> valor = 1;
-            caracter = fgetc(arquivo);
+            //l[cont] -> valor = 1;
+            var_email = fgetc(arquivo);
             
         }
-        adiconar_dados(l, caracter, cont);
-        caracter = fgetc(arquivo);
+        adiconar_dados(var_email, cont, l);
+        var_email = fgetc(arquivo);
     }
     
     fclose (arquivo);
+}
+
+void Ler_arquivo_ipv4(NOLISTA **l){
+    FILE *arquivo_ipv4 = fopen("ipv4.csv","r");
+    int cont = 0;
+    char var_ipv4;
+
+    if (arquivo_ipv4 == NULL){
+        printf("ERRO AO LER O ARQUIVO \n");
+    }
+    else{
+        printf("DEU BOM A LEITURA \n");
+    }
+
+    l[cont] = cria_Lista();
+    //char caracter = fgetc(arquivo);
+
+    while (var_ipv4 != EOF){
+        if (var_ipv4 == ','){
+            cont ++;
+            l[cont] = cria_Lista();
+            //l[cont] -> valor = 1;
+            var_ipv4 = fgetc(arquivo_ipv4);
+            
+        }
+        adiconar_dados(var_ipv4, cont, l);
+        var_ipv4 = fgetc(arquivo_ipv4);
+    }
+    
+    fclose (arquivo_ipv4);
 }
 
 void verifiacar_email(LISTA **l){
@@ -84,21 +112,21 @@ void verifiacar_email(LISTA **l){
     val = 0;
     flag = 0;
 
-    int cont, j, i, k;
-    cont = 0;
+    int j, i, k, m;
+    m = 0;
     j = 0;
     i = 0;
 
     char estudante[23] = {"@estudante.ifms.edu.br"};
-    char professor[23] = {"@ifms.edu.br"};
+    char professor[13] = {"@ifms.edu.br"};
 
     for(i = 0; i < tam; i++){
         NOLISTA *aux = l[i] -> inicio;
 
         while (aux){
-            char *email = (char*)calloc(80, sizeof(char));
+            char *email = (char*)calloc(100, sizeof(char));
 
-            while (aux -> letras != '@'){
+            while (aux->letras != '@'){
                 email[j] = aux->letras;
                 j++;
                 val++;
@@ -113,7 +141,7 @@ void verifiacar_email(LISTA **l){
             }
 
             if(cont_ponto == 1){
-                for(k = 0; k < val -1; cont_ponto++){
+                for(k = 0; k < val -1; k++){
                     if(email[k] >= 'a' && email[k] <= 'z' || email[k] == '.'){
                         exit;
                     }
@@ -122,17 +150,21 @@ void verifiacar_email(LISTA **l){
                     }
                 }
             }
+            else{
+                flag++;
+            }
             free(email);
+            
 
-            if(email != 0){
+            if(flag != 0){
                 l[i] -> valor = 0;
             }
 
-            char *emial_final = (char*)calloc(80, sizeof(char));
+            char *email_final = (char*)calloc(80, sizeof(char));
 
             while (aux != NULL){
-                emial_final[cont] = aux->letras;
-                cont++;
+                email_final[m] = aux->letras;
+                m++;
                 aux = aux->prox;
             }
 
@@ -151,38 +183,59 @@ void verifiacar_email(LISTA **l){
             }
 
             break;
-            free(emial_final);            
+            free(email_final);            
         }
-        printf(l, i);
+        printf("%d, %d",l, i);
     }
         
 }
 
-void imprimir(LISTA **l){
-    NOLISTA *aux;
-    int i;
+void imprimir(LISTA **l, int tam){
+    NOLISTA *aux = l[tam]->inicio;
 
-    for(i = 0; i < 100; i++){
-        aux = l[i] -> inicio;
-
-        printf("inicio->");
-        while (aux != NULL)
-        {
-            printf("%c", aux -> letras);
-            aux = aux -> prox;
-        }
-        printf(" -> final \n");
+    FILE *arq_imp = fopen("resultado_email.csv","w");
+    
+    while (aux){
+        fprintf(arq_imp, "%c", aux->letras);
+        aux = aux->prox;
+    }
+    if(l[tam]->valor == 2){
+        fprintf(arq_imp, "ESTUDADO VALIDO");
+    }
+    if(l[tam]->valor == 3){
+        fprintf(arq_imp,"PROFESSOR VALIDO");
+    }
+    else{
+        fprintf(arq_imp,"EMAIL INVALIDO");
     }
     
 }
 
 int main (void){
 
-    printf("NO MOMENTO O CODIGO ESTA SOMENTE FAZENDO VALIDAÇÃO DE EMIAL");
+    LISTA **var = (LISTA**)malloc(100* sizeof(LISTA*));
 
-    LISTA **v_email = (LISTA**)malloc(100* sizeof(LISTA*));
-    ler_arquivo(v_email);
-    imprimir(v_email);
+    int num_usuario;
+    printf("ESCOLHA UMA DAS OPCOES PARA VERIFICAR PRIMEIRO");    
+    printf("\n1 -> email");
+    printf("\n2 -> ipv4");
+    printf("\n-------------------------------------------------");
+    printf("\nOpcao: ");
+    scanf("%d", &num_usuario);
+
+    if(num_usuario == 1){
+        ler_arquivo_email(var);
+        verifiacar_email(var);
+        printf("\nVALIDACAO CONCLUIDA COM SUCESSO");
+        printf("\nFOI GERADO UM ARQUIVO COM O NOME - resultado_email.csv");
+    }
+    if(num_usuario == 2){
+        Ler_arquivo_ipv4(var);
+        printf("nao ta completa");
+    }
+    else{
+        printf("OPCAO NAO EXISTE");
+    }
 
     return 0;
 }
